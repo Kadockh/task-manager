d8fc040ccb3eec9c19563ba19bf5ccf0add45145
Kadockh <andreykadoch20@gmail.com>
2025-06-17 16:08:12 -0300

    feat: add time section on Tasks component

--- src/_components/Tasks.jsx
+++ src/_components/Tasks.jsx
@@ -1,4 +1,10 @@
-import { AddIcon, TrashIcon } from "../assets/icons"
+import {
+  AddIcon,
+  CloudSunIcon,
+  MoonIcon,
+  SunIcon,
+  TrashIcon,
+} from "../assets/icons"
 import Button from "./Button"
 
 function Tasks() {
@@ -6,12 +12,12 @@ function Tasks() {
     <div className="w-full px-8 py-16">
       <div className="flex justify-between">
         <div>
-          <span className="text-xs font-semibold text-gray-500">
+          <span className="text-xs font-semibold text-[#00adb5]">
             Minhas Tarefas
           </span>
-          <h1 className="text-3xl font-semibold">Minhas Tarefas</h1>
+          <h1 className="text-xl font-semibold">Minhas Tarefas</h1>
         </div>
-        <div className="flex items-center gap-2">
+        <div className="flex items-center gap-3">
           <Button variant="ghost">
             <TrashIcon />
             Limpar tarefas
@@ -21,6 +27,29 @@ function Tasks() {
             <AddIcon />
             Adicionar tarefa
           </Button>
+        </div>
+      </div>
+
+      <div className="rounded-xl bg-white p-6">
+        <div className="space-y-3">
+          <div className="flex gap-2 border-b border-solid border-[#f4f4f5] pb-1">
+            <SunIcon />
+            <p className="text-sm text-[#9a9c9f]">Manhã</p>
+          </div>
+        </div>
+
+        <div className="my-6 space-y-3">
+          <div className="flex gap-2 border-b border-solid border-[#f4f4f5] pb-1">
+            <CloudSunIcon />
+            <p className="text-sm text-[#9a9c9f]">Tarde</p>
+          </div>
+        </div>
+
+        <div className="space-y-3">
+          <div className="flex gap-2 border-b border-solid border-[#f4f4f5] pb-1">
+            <MoonIcon />
+            <p className="text-sm text-[#9a9c9f]">Noite</p>
+          </div>
         </div>
       </div>
     </div>
