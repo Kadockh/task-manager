7d423231c3be889c0081dbeee5860b0764b911ef
Kadockh <andreykadoch20@gmail.com>
2025-06-23 20:20:19 -0300

    chore: add prop-types to Header component

--- src/_components/Header.jsx
+++ src/_components/Header.jsx
@@ -1,5 +1,11 @@
-function Header(props) {
+import PropTypes from "prop-types"
+
+function Header({ props }) {
   return <header className="header">{props.children}</header>
+}
+
+Header.propTypes = {
+  children: PropTypes.node.isRequired,
 }
 
 export default Header
